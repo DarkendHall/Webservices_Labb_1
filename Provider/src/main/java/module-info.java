03a655097ceaeaaddbce5a0f8055f846a978eb45
MarcusLeeman <marcus.leeman@iths.se>
2022-04-20 15:10:05 +0200

    Add class: EUR

--- Provider/src/main/java/module-info.java
+++ Provider/src/main/java/module-info.java
@@ -1,8 +1,9 @@
 import org.darkend.converter.CurrencyConverter;
+import org.darkend.provider.EUR;
 import org.darkend.provider.USD;
 
 module org.darkend.provider {
     requires org.darkend.converter;
     requires java.net.http;
-    provides CurrencyConverter with USD;
+    provides CurrencyConverter with USD, EUR;
 }
---
+++ Provider/src/main/java/org/darkend/provider/EUR.java
@@ -0,0 +1,63 @@
+package org.darkend.provider;
+
+import org.darkend.converter.CurrencyConverter;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class EUR implements CurrencyConverter {
+
+    private final Double currentRateToEUR;
+    private final Double currentRateFromEUR;
+
+    public EUR() {
+        try {
+            var client = HttpClient.newHttpClient();
+            var request = HttpRequest.newBuilder(URI.create(
+                            "https://free.currconv.com/api/v7/convert?q=SEK_EUR&compact=ultra&apiKey=" + System.getenv(
+                                    "API_KEY")))
+                    .GET()
+                    .build();
+            var response = client.send(request, HttpResponse.BodyHandlers.ofString())
+                    .body();
+            var responseStringArray = response.split(":");
+            var responseNumberAsString = responseStringArray[1];
+            currentRateToEUR = Double.parseDouble(responseNumberAsString.replace("}", ""));
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            var client = HttpClient.newHttpClient();
+            var request = HttpRequest.newBuilder(URI.create(
+                            "https://free.currconv.com/api/v7/convert?q=EUR_SEK&compact=ultra&apiKey=" + System.getenv(
+                                    "API_KEY")))
+                    .GET()
+                    .build();
+            var response = client.send(request, HttpResponse.BodyHandlers.ofString())
+                    .body();
+            var responseStringArray = response.split(":");
+            var responseNumberAsString = responseStringArray[1];
+            currentRateFromEUR = Double.parseDouble(responseNumberAsString.replace("}", ""));
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public double convertTo(double sek) {
+        return sek * currentRateToEUR;
+    }
+
+    @Override
+    public double convertFrom(double eur) {
+        return eur * currentRateFromEUR;
+    }
+
+    @Override
+    public String currency() {
+        return "EUR";
+    }
+}
